aa50f564f3e5a4b930965031af0ffb36aabed3ff
subsetsurprise <tstacey@pivotal.io>
2020-03-28 10:38:07 -0400

    Update shell scripts

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -15,7 +15,7 @@ if [ -z "$VM_FOLDER" ]; then
   govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
-    govc folder.create "$OM_VM_FOLDER"
+    govc folder.create "$VM_FOLDER"
   fi
   govc import.ova -folder="$VM_FOLDER" "$file_path" -folder="$VM_FOLDER" -name=base-os-latest-ova
   govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) -folder="$VM_FOLDER" base-os-latest
