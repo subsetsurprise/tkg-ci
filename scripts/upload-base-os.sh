01d57f0f735e06fd1809b643c928410231f3fbb3
subsetsurprise <tstacey@pivotal.io>
2020-03-28 10:34:56 -0400

    Update shell scripts with VM names

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -11,12 +11,12 @@ export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  govc import.ova "$file_path"
+  govc import.ova "$file_path" -folder="$VM_FOLDER" -name=base-os-latest-ova
   govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$OM_VM_FOLDER"
   fi
-  govc import.ova -folder="$VM_FOLDER" "$file_path"
-  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
+  govc import.ova -folder="$VM_FOLDER" "$file_path" -folder="$VM_FOLDER" -name=base-os-latest-ova
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) -folder="$VM_FOLDER" base-os-latest
 fi
--- scripts/upload-ha-proxy.sh
+++ scripts/upload-ha-proxy.sh
@@ -10,12 +10,12 @@ export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  govc import.ova "$file_path"
-  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) ha-proxy-latest
+  govc import.ova "$file_path" -folder="$VM_FOLDER" -name=ha-proxy-latest-ova
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) -folder="$VM_FOLDER" ha-proxy-latest
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$OM_VM_FOLDER"
   fi
-  govc import.ova -folder="$VM_FOLDER" "$file_path"
-  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) ha-proxy-latest
+  govc import.ova -folder="$VM_FOLDER" "$file_path" -folder="$VM_FOLDER" -name=ha-proxy-latest-ova
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) -folder="$VM_FOLDER" ha-proxy-latest
 fi
