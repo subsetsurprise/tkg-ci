fa5f99e961f5bfd19e8bd0f22bee7563b8e97177
subsetsurprise <tstacey@pivotal.io>
2020-03-28 12:20:15 -0400

    Fixed HA Proxy script

--- scripts/upload-ha-proxy.sh
+++ scripts/upload-ha-proxy.sh
@@ -3,7 +3,7 @@
 set -eu
 
 echo "Upload of the HA Proxy OVA has started."
-file_path=$(find ./base-os-ova/ -name "*.ova")
+file_path=$(find ./ha-proxy-ova/ -name "*.ova")
 
 echo "$file_path"
 
@@ -12,23 +12,31 @@ echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
   if govc vm.info -r ha-proxy-latest-ova | grep -q Name: ; then
-    govc vm.clone -vm ha-proxy-latest-ova ha-proxy-latest
+    if govc vm.info -r ha-proxy-latest | grep -q Name: ; then
     exit 0
+    else
+      govc vm.clone -vm ha-proxy-latest-ova -on false ha-proxy-latest
+      exit 0
+    fi
   else
     govc import.ova -folder="$VM_FOLDER" -name ha-proxy-latest-ova "$file_path" 
-    govc vm.clone -vm ha-proxy-latest-ova ha-proxy-latest
+    govc vm.clone -vm ha-proxy-latest-ova -on false ha-proxy-latest
     exit 0
   fi
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$VM_FOLDER"
   fi
-  if govc import.ova -folder "$VM_FOLDER" -name ha-proxy-latest-ova "$file_path" | grep -q 'govc: The name 'ha-proxy-latest-ova' already exists.'; then
-    govc vm.clone -vm ha-proxy-latest-ova ha-proxy-latest
+  if govc vm.info -r ha-proxy-latest-ova | grep -q Name: ; then
+    if govc vm.info -r ha-proxy-latest | grep -q Name: ; then
     exit 0
+    else
+      govc vm.clone -vm ha-proxy-latest-ova -on false ha-proxy-latest
+      exit 0
+    fi
   else
     govc import.ova -folder="$VM_FOLDER" -name ha-proxy-latest-ova "$file_path" 
-    govc vm.clone -vm ha-proxy-latest-ova ha-proxy-latest
+    govc vm.clone -vm ha-proxy-latest-ova -on false ha-proxy-latest
     exit 0
   fi
 fi
