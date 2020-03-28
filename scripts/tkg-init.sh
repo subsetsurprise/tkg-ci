d1bf03e884a7c46515e6f4dc1c3066b0751fc711
subsetsurprise <tstacey@pivotal.io>
2020-03-28 14:03:01 -0400

    Fixed TKG script

--- scripts/tkg-init.sh
+++ scripts/tkg-init.sh
@@ -10,10 +10,11 @@ chmod +x /bin/tkg
 
 config_path=$(find ./tkg-ci/ -name "config.yml")
 tkg init || cat "$config_path" >> /root/.tkg/config.yaml
+echo "" >> /root/.tkg/config.yaml
 echo "VSPHERE_SERVER: $VSPHERE_SERVER" >> /root/.tkg/config.yaml
 echo "VSPHERE_USERNAME: $VSPHERE_USERNAME" >> /root/.tkg/config.yaml
 echo "VSPHERE_PASSWORD: $VSPHERE_PASSWORD" >> /root/.tkg/config.yaml
-echo "" >> /root/.tkg/config.yaml
+
 cat /root/.tkg/config.yaml
 
 tkg init --infrastructure=vsphere --plan=prod
