22d36c09ca31e8567745c5424af10fc717d0028c
subsetsurprise <tstacey@pivotal.io>
2020-03-28 11:28:33 -0400

    Update shell scripts with VM names

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -11,12 +11,12 @@ export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  govc import.ova -folder="$VM_FOLDER" -name=base-os-latest-ova "$file_path"
+  govc import.ova -folder "$VM_FOLDER" -name base-os-latest-ova "$file_path"
   govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$VM_FOLDER"
   fi
   govc import.ova -folder="$VM_FOLDER" -name=base-os-latest-ova "$file_path" 
-  govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) -folder="$VM_FOLDER" base-os-latest
+  govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
 fi
