3b04ea5ea75977a7d36da48b6d44e848d7c3a9c5
subsetsurprise <tstacey@pivotal.io>
2020-03-28 11:54:46 -0400

    Test vm.info

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -6,12 +6,13 @@ echo "I made it into the script"
 file_path=$(find ./base-os-ova/ -name "*.ova")
 
 echo "$file_path"
+govc vm.info base-os-latest-ova
 
 export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  if govc import.ova -folder "$VM_FOLDER" -name base-os-latest-ova "$file_path" | grep -q 'govc: The name 'base-os-latest-ova' already exists.'; then
+  if govc | grep -q 'govc: The name 'base-os-latest-ova' already exists.'; then
     govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
   else
     govc import.ova -folder="$VM_FOLDER" -name base-os-latest-ova "$file_path" 
