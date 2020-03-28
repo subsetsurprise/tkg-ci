94cdfe1907bc1ef058cc1bffbbb2973d2ba4af20
subsetsurprise <tstacey@pivotal.io>
2020-03-28 11:47:45 -0400

    Improve error checking with govc

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -11,12 +11,20 @@ export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  govc import.ova -folder "$VM_FOLDER" -name base-os-latest-ova "$file_path"
-  govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  if govc import.ova -folder "$VM_FOLDER" -name base-os-latest-ova "$file_path" | grep -q 'govc: The name 'base-os-latest-ova' already exists.'; then
+    govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  else
+    govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
+    govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  fi
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$VM_FOLDER"
   fi
-  govc import.ova -folder="$VM_FOLDER" -name=base-os-latest-ova "$file_path" 
-  govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  if govc import.ova -folder "$VM_FOLDER" -name base-os-latest-ova "$file_path" | grep -q 'govc: The name 'base-os-latest-ova' already exists.'; then
+    govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  else
+    govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
+    govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  fi
 fi
--- scripts/upload-ha-proxy.sh
+++ scripts/upload-ha-proxy.sh
@@ -10,12 +10,20 @@ export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
-  govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
+  if govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" | grep -q 'govc: The name 'base-os-latest-ova' already exists.'; then
+    govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
+  else
+    govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
+    govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
+  fi
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$VM_FOLDER"
   fi
-  govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
-  govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
+  if govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" | grep -q 'govc: The name 'base-os-latest-ova' already exists.'; then
+    govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
+  else
+    govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
+    govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
+  fi
 fi
