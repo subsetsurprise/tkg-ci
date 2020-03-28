75bd7dd060161932ae53c4fcf52c8a1db9db3fd3
subsetsurprise <tstacey@pivotal.io>
2020-03-28 10:17:22 -0400

    Renamed OM ENV

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -10,13 +10,13 @@ echo "$file_path"
 export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
-if [ -z "$OM_VM_FOLDER" ]; then
+if [ -z "$VM_FOLDER" ]; then
   govc import.ova "$file_path"
   govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
 else
-  if [ "$(govc folder.info "$OM_VM_FOLDER" 2>&1 | grep "$OM_VM_FOLDER" | awk '{print $2}')" != "$OM_VM_FOLDER" ]; then
+  if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$OM_VM_FOLDER"
   fi
-  govc import.ova -folder="$OM_VM_FOLDER" "$file_path"
+  govc import.ova -folder="$VM_FOLDER" "$file_path"
   govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
 fi
