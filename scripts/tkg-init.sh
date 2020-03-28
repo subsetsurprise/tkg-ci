c2c130eea63ca58bbfa9dc348df239d55dda0c17
subsetsurprise <tstacey@pivotal.io>
2020-03-28 14:26:33 -0400

    added fix-install option

--- scripts/tkg-init.sh
+++ scripts/tkg-init.sh
@@ -9,11 +9,11 @@ cp "$file_path" /bin/tkg
 chmod +x /bin/tkg
 
 apt-get update
-apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt-get -y --fix-broken install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
-apt-get -y install docker-ce
+apt-get -y --fix-broken install docker-ce
 alias docker="docker run -v /var/run/docker.sock:/var/run/docker.sock"
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
