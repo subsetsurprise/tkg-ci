ef3327d1a34fd2797ad65f1152526d297c0bb17c
subsetsurprise <tstacey@pivotal.io>
2020-03-28 10:58:21 -0400

    Update shell scripts

--- scripts/upload-ha-proxy.sh
+++ scripts/upload-ha-proxy.sh
@@ -11,11 +11,11 @@ echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
   govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
-  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) -folder="$VM_FOLDER" ha-proxy-latest
+  govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
     govc folder.create "$VM_FOLDER"
   fi
   govc import.ova -folder="$VM_FOLDER" -name=ha-proxy-latest-ova "$file_path" 
-  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) -folder="$VM_FOLDER" ha-proxy-latest
+  govc vm.clone -vm ha-proxy-latest-ova -snapshot $(govc snapshot.tree -vm ha-proxy-latest-ova -C) -folder="$VM_FOLDER" ha-proxy-latest
 fi
