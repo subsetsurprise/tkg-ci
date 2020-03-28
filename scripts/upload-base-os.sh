cf08fbdbf0a329ed70ea092c6800614a5bb0bbb0
subsetsurprise <tstacey@pivotal.io>
2020-03-28 10:15:24 -0400

    Removed naming convention

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -4,10 +4,7 @@ set -eu
 
 echo "I made it into the script"
 file_path=$(find ./base-os-ova/ -name "*.ova")
-uuid=$(date | md5sum)
-name="base-os-" + "$uuid"
 
-echo "$name"
 echo "$file_path"
 
 export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
@@ -15,11 +12,11 @@ echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$OM_VM_FOLDER" ]; then
   govc import.ova "$file_path"
-  govc vm.clone -vm "$name" -snapshot $(govc snapshot.tree -vm "$name" -C) base-os-latest
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
 else
   if [ "$(govc folder.info "$OM_VM_FOLDER" 2>&1 | grep "$OM_VM_FOLDER" | awk '{print $2}')" != "$OM_VM_FOLDER" ]; then
     govc folder.create "$OM_VM_FOLDER"
   fi
   govc import.ova -folder="$OM_VM_FOLDER" "$file_path"
-  govc vm.clone -vm "$name" -snapshot $(govc snapshot.tree -vm "$name" -C) base-os-latest
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) base-os-latest
 fi
--- scripts/upload-ha-proxy.sh
+++ scripts/upload-ha-proxy.sh
@@ -3,10 +3,7 @@
 set -eu
 
 file_path=$(find ./ha-proxy-ova/ -name "*.ova")
-uuid=$(date | md5sum)
-name="ha-proxy-" + "$uuid"
 
-echo "$name"
 echo "$file_path"
 
 export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
@@ -14,11 +11,11 @@ echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$OM_VM_FOLDER" ]; then
   govc import.ova "$file_path"
-  govc vm.clone -vm "$name" -snapshot $(govc snapshot.tree -vm "$name" -C) ha-proxy-latest
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) ha-proxy-latest
 else
   if [ "$(govc folder.info "$OM_VM_FOLDER" 2>&1 | grep "$OM_VM_FOLDER" | awk '{print $2}')" != "$OM_VM_FOLDER" ]; then
     govc folder.create "$OM_VM_FOLDER"
   fi
   govc import.ova -folder="$OM_VM_FOLDER" "$file_path"
-  govc vm.clone -vm "$name" -snapshot $(govc snapshot.tree -vm "$name" -C) ha-proxy-latest
+  govc vm.clone -vm "$file_path" -snapshot $(govc snapshot.tree -vm "$file_path" -C) ha-proxy-latest
 fi
