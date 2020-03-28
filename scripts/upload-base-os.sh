fd4bf7d0f3754a86be357cc7c7677553c9d169d5
subsetsurprise <tstacey@pivotal.io>
2020-03-28 12:08:36 -0400

    Modifdied vm.clone command

--- scripts/upload-base-os.sh
+++ scripts/upload-base-os.sh
@@ -6,17 +6,16 @@ echo "Upload of the Base OS OVA has started."
 file_path=$(find ./base-os-ova/ -name "*.ova")
 
 echo "$file_path"
-govc vm.info -r base-os-latest-ova | grep Name: | grep base-os-latest-ova
 
 export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
-  if govc vm.info -r base-os-latest-ova | grep Name: | grep -q 'base-os-latest-ova'; then
-    govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+  if govc vm.info -r base-os-latest-ova | grep -q Name: ; then
+    govc vm.clone -vm base-os-latest-ova base-os-latest
   else
     govc import.ova -folder="$VM_FOLDER" -name base-os-latest-ova "$file_path" 
-    govc vm.clone -vm base-os-latest-ova -snapshot $(govc snapshot.tree -vm base-os-latest-ova -C) base-os-latest
+    govc vm.clone -vm base-os-latest-ova base-os-latest
   fi
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "$VM_FOLDER" | awk '{print $2}')" != "$VM_FOLDER" ]; then
