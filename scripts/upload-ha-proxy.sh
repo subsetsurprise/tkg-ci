f198b2758899124e9001c2322f08286cbd6f3825
subsetsurprise <tstacey@pivotal.io>
2020-03-28 13:25:25 -0400

    Modified HA Proxy Script

--- scripts/upload-ha-proxy.sh
+++ scripts/upload-ha-proxy.sh
@@ -12,9 +12,10 @@ echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
 
 if [ -z "$VM_FOLDER" ]; then
   if govc vm.info -r ha-proxy-latest-ova | grep -q Name: ; then
+    exit 0
+  else
     echo "The OVA is being imported."
     govc import.ova -folder="$VM_FOLDER" -name ha-proxy-latest-ova "$file_path" 
-    exit 0
   fi
 else
   if [ "$(govc folder.info "$VM_FOLDER" 2>&1 | grep "Name:" | awk '{print $2}')" != "$VM_FOLDER" ]; then
